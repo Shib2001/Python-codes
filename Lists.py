6cba6ad5f405a9c920c5d1b07548fdbafef42662
Shiv kumar Jha <shivkumarjha@techgropse.com>
2026-07-26 22:55:08 +0530

    lists in python

---
+++ Lists.py
@@ -0,0 +1,12 @@
+#list is a set of data type that stores set of values , something similar to array 
+
+marks = [ 778,66746,4743843,57848754]
+
+student = ["karan", 87878, "jhshdsj"]
+
+student[0]="arjun"
+
+print(student)
+
+
+#IMPORTANT POINT : STRINGS ARE IMMUTABLE , WHILE LIST ARE MUTABLE 
