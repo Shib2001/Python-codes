9619d50a86a2bf538b4949b5d08a50fb7d545944
Shiv Kumar Jha <shivkumarjha@techgropse.com>
2026-09-03 13:42:05 +0530

    loops

---
+++ Loops.py
@@ -0,0 +1,33 @@
+#This was the example of while loop , Now loop ke chakkar lgane ko iteration kehet h , ek iteration yaaani ek chaakar lgaan of the loop and the cycle goes on 
+
+
+# count = 1
+# while count <= 10:
+#     print(count)
+
+#another example of while loop
+
+# i = 5 
+# while i <= 15:
+#     print("this counting is working fine")
+#     i += 1 # this is for iteration
+# 
+# 
+# some questions 
+# 
+# Print numbers from 1 to 100 
+
+
+# i = 1 
+# while i <=100:
+#     print("current number is ", i)
+#     i += 1 
+
+#___________________________________________________________________________________________________________________________________________
+
+#print numbers from 100 to 1 
+# i = 100
+# while i >=1:
+#     print("amit ki kitni bandiya hai", i)
+#     i -= 1
+    
