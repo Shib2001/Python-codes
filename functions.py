ebe2cee7982a22ac07bff4785c4325bf7eec31ba
Shiv kumar Jha <shivkumarjha@techgropse.com>
2026-09-06 23:46:58 +0530

    functions and some practice questions

---
+++ functions.py
@@ -0,0 +1,32 @@
+# def sum (a, b):
+#     s = a+b
+#     print(sum)
+#     return sum
+
+#Now we can call this function any time here 
+
+# sum(5, 7)
+
+
+# sum(19341,4329483)
+
+
+# another thing that we can do 
+
+# def tmkc(a,b):
+#     return a - b
+
+# bsdk = tmkc(4,6)
+# print(bsdk)
+
+
+# create a function that calculates a average of 3 numbers 
+
+def lund(a,b,c):
+    avg =  a+b+c
+    avg2 = avg / 3
+    print(avg2)
+    return avg2
+
+
+lund(348834,43899843,43989834)
